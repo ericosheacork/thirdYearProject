410cc5b928b15af86222d7f0c631b50c137a5528
Jonathan Lim <j.lim-kean-lee@mycit.ie>
2019-11-21 10:12:29 +0000

    Added authentication functionality to web application.

---
+++ webwork/public/doctor_credentials.js
@@ -0,0 +1,12 @@
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCIrRQLlsAzssCKjevk9AwFloPLrWSCfHk",
+  authDomain: "mediscreen-674eb.firebaseapp.com",
+  databaseURL: "https://mediscreen-674eb.firebaseio.com",
+  projectId: "mediscreen-674eb",
+  storageBucket: "mediscreen-674eb.appspot.com",
+  messagingSenderId: "292545820886",
+  appId: "1:292545820886:web:9fe5753b475a2f524b3177",
+  measurementId: "G-FCP7TQ40TP"
+};
+firebase.initializeApp(firebaseConfig);
