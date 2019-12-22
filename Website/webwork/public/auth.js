0c8ee51ec168010e1dc07a64e254b5487e70e44c
Jonathan Lim <j.lim-kean-lee@mycit.ie>
2019-12-22 17:55:11 +0000

    Newly registered doctors now added to database

--- Website/webwork/public/auth.js
+++ Website/webwork/public/auth.js
@@ -5,9 +5,9 @@ const outputPatients = (data) =>{
   data.forEach(doc =>{
     const patient = doc.data();
     const entry = `<li>
-    <div id="patient_fname">${patient.First_Name}</div>
-    <div id="patient_lname">${patient.Last_Name}</div>
-    <div id="patient_email">${patient.Email}</div>
+    <div id="patient_fname">First Name: ${patient.First_Name}</div>
+    <div id="patient_lname">Last Name: ${patient.Last_Name}</div>
+    <div id="patient_email">Email: ${patient.Email}</div><br>
     </li>`;
     html += entry;
     //console.log(patient);
